777b25552b471a2fe9349f13e5a4e492495814ab
lukematthewsh <54856144+lukematthewsh@users.noreply.github.com>
2020-02-13 11:49:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function ask(questionText) {
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 
+//--------------------------------------------------------------------------------------------------
+
 let states = {
   'Main St': { canChangeTo: ['Muddy Waters', 'Mr. Mikes', '182 Main St'] },
   '182 Main St': { canChangeTo: ['Foyer', 'Main St'] },
@@ -20,10 +22,6 @@ let states = {
   'Mr Mikes': { canChangeTo: ['Main St'] }
 };
 //Updated State Machine to handle our rooms. 
-
-//--------------------------------------------------------------------------------------------------
-
-
 
 function moveToRoom(newRoom) {
   let validTransitions = states[currentRoom.name].canChangeTo;
@@ -35,33 +33,37 @@ function moveToRoom(newRoom) {
 }
 
 //--------------------------------------------------------------------------------------------------
+
 // Created the basic room objects. Will need to add more to these
 // player object
 // room directions
+
+
+//--------------------------------------------------------------------------------------------------
+
 let player = {
   health: 100,
   status: function(){
 // status is going to help determine players energy level or hunger level
   }
-
 }
+
+//--------------------------------------------------------------------------------------------------
+// Rooms //
+
 class Room {
   constructor(name, message, north, south, east, west, locked = false) {
     this.name = name
     this.message = message
     this.locked = locked
-
-    this.north = north
 // north south east and west are going to help our character move. I think there needs to be a function involved
+// This could be used instead of state machine. We can decide what we like better.
+    this.north = north
     this.south = south
-
     this.east = east
-
     this.west = west
   }
-
 }
-
 
 const room1 = new Room('182 Main St', "You're at 182 Main St", true)
 const room2 = new Room('Foyer', "You're in the Foyer")
@@ -70,7 +72,11 @@ const room4 = new Room("Muddy Waters", "You're at Muddy Waters")
 const room5 = new Room("Mr Mikes", "You're at Mr Mikes")
 const room6 = new Room("Main St", "You're on Main St")
 
+//--------------------------------------------------------------------------------------------------
+// Other Objects that need to be interacted with //
+
 const sign = {message: "The password is 12345"}
+
 //--------------------------------------------------------------------------------------------------
 
 let currentRoom = room1;
@@ -88,7 +94,7 @@ On the door is a handwritten sign.`;
   console.log('Now write your code to make this work!');
   process.exit();
 
-
+//--------------------------------------------------------------------------------------------------
 
 function locked() {
   if (Room.locked == true)
