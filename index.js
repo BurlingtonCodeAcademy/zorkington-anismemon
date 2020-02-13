8668b96d0b80c624353a509e3f1e2852c057e57e
Luke Matthews <lukematthewsh@gmail.com>
2020-02-13 11:26:36 -0500

    Changerd object to class constructor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
@@ -11,12 +12,12 @@ function ask(questionText) {
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 
 let states = {
-  'Main St': { canChangeTo: ['Muddy Waters', 'Mr. Mikes', '182 Main St' ] },
-  '182 Main St': { canChangeTo: ['Foyer', 'Main St']},
-  'Foyer': { canChangeTo: [ 'Classroom', '182 Main St' ] },
-  'Classroom': { canChangeTo: [ 'Foyer' ] },
-  'Muddy Waters' : {canChangeTo: ['Main St']},
-  'Mr Mikes' : {canChangeTo: ['Main St']}
+  'Main St': { canChangeTo: ['Muddy Waters', 'Mr. Mikes', '182 Main St'] },
+  '182 Main St': { canChangeTo: ['Foyer', 'Main St'] },
+  'Foyer': { canChangeTo: ['Classroom', '182 Main St'] },
+  'Classroom': { canChangeTo: ['Foyer'] },
+  'Muddy Waters': { canChangeTo: ['Main St'] },
+  'Mr Mikes': { canChangeTo: ['Main St'] }
 };
 //Updated State Machine to handle our rooms. 
 
@@ -35,25 +36,46 @@ function moveToRoom(newRoom) {
 
 //--------------------------------------------------------------------------------------------------
 // Created the basic room objects. Will need to add more to these
-class Room{}
+// player object
+// room directions
+let player = {
+  health: 100,
+  status: function(){
+// status is going to help determine players energy level or hunger level
+  }
 
-let room1 = new Room
-  room1.name = '182 Main St'
-let room2 = new Room
-  room2.name = 'Foyer'
-let room3 = new Room
-  room3.name = 'Classroom'
-let room4 = new Room
-  room4.name = 'Muddy Waters'
-let room5 = new Room
-  room5.name = 'Mr Mikes'
-let room6 = new Room
-  room6.name = 'Main St'
+}
+class Room {
+  constructor(name, message, north, south, east, west, locked = false) {
+    this.name = name
+    this.message = message
+    this.locked = locked
 
+    this.north = north
+// north south east and west are going to help our character move. I think there needs to be a function involved
+    this.south = south
+
+    this.east = east
+
+    this.west = west
+  }
+
+}
+
+
+const room1 = new Room('182 Main St', "You're at 182 Main St", true)
+const room2 = new Room('Foyer', "You're in the Foyer")
+const room3 = new Room('Classroom', "You're in the Classroom")
+const room4 = new Room("Muddy Waters", "You're at Muddy Waters")
+const room5 = new Room("Mr Mikes", "You're at Mr Mikes")
+const room6 = new Room("Main St", "You're on Main St")
+
+const sign = {message: "The password is 12345"}
 //--------------------------------------------------------------------------------------------------
 
 let currentRoom = room1;
 start();
+locked();
 
 async function start() {
   const welcomeMessage = `182 Main St.
@@ -62,6 +84,13 @@ There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.`;
   let answer = await ask(welcomeMessage);
 
+}
   console.log('Now write your code to make this work!');
   process.exit();
+
+
+
+function locked() {
+  if (Room.locked == true)
+    console.log(chalk.bold.red('Bzzzzt Locked!'))
 }
