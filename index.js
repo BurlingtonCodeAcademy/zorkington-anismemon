a2d0584891c1052e1d274af97d7052563d18ce5a
Luke Matthews <lukematthewsh@gmail.com>
2020-02-13 09:02:50 -0500

    Added rooms and updated the state machine

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ function ask(questionText) {
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
+  'Main St': { canChangeTo: ['Muddy Waters', 'Mr. Mikes', '182 Main St' ] },
+  '182 Main St': { canChangeTo: ['Foyer', 'Main St']},
+  'Foyer': { canChangeTo: [ 'Classroom', '182 Main St' ] },
+  'Classroom': { canChangeTo: [ 'Foyer' ] },
+  'Muddy Waters' : {canChangeTo: ['Main St']},
+  'Mr Mikes' : {canChangeTo: ['Main St']}
 };
+//Updated State Machine to handle our rooms. 
 
-let currentState = "green";
+//--------------------------------------------------------------------------------------------------
 
-function enterState(newState) {
+let currentRoom = "room1";
+
+function e(newState) {
   let validTransitions = states[currentState].canChangeTo;
   if (validTransitions.includes(newState)) {
     currentState = newState;
@@ -25,6 +31,36 @@ function enterState(newState) {
     throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
   }
 }
+
+
+//--------------------------------------------------------------------------------------------------
+// Created the basic room objects. Will need to add more to these
+
+let room1 = {
+  name : "182 Main St",
+}
+let room2 = {
+  name: "Foyer"
+}
+
+let room3 = {
+  name : "Classroom"
+}
+
+let room4 = {
+  name : "Muddy Waters"
+}
+
+let room5 = {
+  name : "Mr. Mikes"
+}
+
+let room6 = {
+  name : "Main St"
+}
+
+//--------------------------------------------------------------------------------------------------
+
 
 start();
 
