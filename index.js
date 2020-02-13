560905ebd7a2fbd03aba1d5f36d7b2164552c313
Luke Matthews <lukematthewsh@gmail.com>
2020-02-13 09:47:03 -0500

    Changed room objects to a room class

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function ask(questionText) {
 
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
+
 let states = {
   'Main St': { canChangeTo: ['Muddy Waters', 'Mr. Mikes', '182 Main St' ] },
   '182 Main St': { canChangeTo: ['Foyer', 'Main St']},
@@ -21,7 +22,7 @@ let states = {
 
 //--------------------------------------------------------------------------------------------------
 
-let currentRoom = "room1";
+
 
 function moveToRoom(newRoom) {
   let validTransitions = states[currentRoom.name].canChangeTo;
@@ -32,36 +33,26 @@ function moveToRoom(newRoom) {
   }
 }
 
-
 //--------------------------------------------------------------------------------------------------
 // Created the basic room objects. Will need to add more to these
+class Room{}
 
-let room1 = {
-  name : "182 Main St",
-}
-let room2 = {
-  name: "Foyer"
-}
-
-let room3 = {
-  name : "Classroom"
-}
-
-let room4 = {
-  name : "Muddy Waters"
-}
-
-let room5 = {
-  name : "Mr. Mikes"
-}
-
-let room6 = {
-  name : "Main St"
-}
+let room1 = new Room
+  room1.name = '182 Main St'
+let room2 = new Room
+  room2.name = 'Foyer'
+let room3 = new Room
+  room3.name = 'Classroom'
+let room4 = new Room
+  room4.name = 'Muddy Waters'
+let room5 = new Room
+  room5.name = 'Mr Mikes'
+let room6 = new Room
+  room6.name = 'Main St'
 
 //--------------------------------------------------------------------------------------------------
 
-
+let currentRoom = room1;
 start();
 
 async function start() {
@@ -70,6 +61,7 @@ You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.`;
   let answer = await ask(welcomeMessage);
+
   console.log('Now write your code to make this work!');
   process.exit();
 }
