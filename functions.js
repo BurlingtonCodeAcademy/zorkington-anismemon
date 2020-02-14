1f603a6f558c540fc1b6c07326f2137017cd6db4
Luke Matthews <lukematthewsh@gmail.com>
2020-02-14 15:50:03 -0500

    Need to finish up

---
+++ functions.js
@@ -0,0 +1,44 @@
+// take function to allow player to pick up items from rooms and add to own inventory
+
+userInput = "take rock"
+
+let roomInv = ["table", "chair", "rock", "lamp"]
+let playerInv = ["paper"]
+
+function takeItem() {
+    let inputArray = userInput.split(" ")
+    console.log(inputArray)
+    let item = inputArray[1]
+    console.log(item)
+    let n = roomInv.indexOf(item)
+    console.log(n)
+    let arrayItem = roomInv.splice(n, 1)
+    let roomItem = arrayItem.join()
+    playerInv.push(roomItem)
+    console.log(playerInv)
+    console.log(roomInv)
+}
+takeItem()
+
+// drop function to allow player to drop an item from his / her inventory, which is added to current room inventory
+
+userInput = "drop rock"
+
+let roomInv = ["table", "chair", "lamp"]
+let playerInv = ["paper", "rock"]
+
+function dropItem() {
+    let inputArray = userInput.split(" ")
+    console.log(inputArray)
+    let item = inputArray[1]
+    console.log(item)
+    let n = playerInv.indexOf(item)
+    console.log(n)
+    let arrayItem = playerInv.splice(n, 1)
+    let playerItem = arrayItem.join()
+    roomInv.push(playerItem)
+    console.log(playerInv)
+    console.log(roomInv)
+}
+dropItem()
+
--- index.js
+++ index.js
@@ -7,7 +7,6 @@
 
 // Figure out how to remove items from arrays \\
 // indexof splice \\ 
-// Add general game insturctions at beginning \\
 
 // Read paper at muddy waters + 2 health points\\
 
@@ -28,10 +27,10 @@ function ask(questionText) {
 //--------------------------------------------------------------------------------------------------
 
 let states = {
-  'MainSt1': { canChangeTo: ['MainSt2', '182 Main St', 'MainSt3'] },
-  'MainSt2': { canChangeTo: ['MainSt1', 'MainSt3', 'Muddy Waters'] },
-  'MainSt3': { canChangeTo: ['MainSt2', 'Mr Mikes', "MainSt1"] },
-  '182 Main St': { canChangeTo: ['Foyer', 'MainSt1'] },
+  //'MainSt1': { canChangeTo: ['MainSt2', '182 Main St', 'MainSt3'] },
+  'MainSt2': { canChangeTo: ['182 Main St', 'MainSt3', 'Muddy Waters'] },
+  'MainSt3': { canChangeTo: ['MainSt2', 'Mr Mikes', "182 Main St"] },
+  '182 Main St': { canChangeTo: ['Foyer', 'MainSt2', "MainSt3"] },
   'Foyer': { canChangeTo: ['Classroom', '182 Main St'] },
   'Classroom': { canChangeTo: ['Foyer'] },
   'Muddy Waters': { canChangeTo: ['MainSt2'] },
@@ -61,21 +60,22 @@ class Room {
   }
 }
 
-const room1 = new Room('182 Main St', "\nYou're at 182 Main St.,\nthe home of the Burlington Code Academy!", ["rock"], true)
-const room2 = new Room('Foyer', "\nYou're in the foyer.\nThe foyer is a foyer.\nThere are stairs infront of you.\nThe door to 182 Main St. is behind you.\nIt is not locked.", "", true)
-const room3 = new Room('Classroom', "\nYou're in the Classroom.\nBob, the instructor, is not currently here.\nHe's probably either making tea or skiing.\nYou could waild for him, but it might be a long wait.\nYou can go back down stairs, or you can stay here.", "")
+const room1 = new Room('182 Main St', "\nYou're at 182 Main St.\nThe home of the Burlington Code Academy!\n\nAhead of you is a door.\nDown the street is Mr Mikes and Muddy Waters.\nOn the door is a hand written sign", ["rock", "coin", "chewed gum"], true)
+const room2 = new Room('Foyer', "\nYou're in the foyer.\nThe foyer is a foyer.\nThere are stairs ahead of you.\nThe door to 182 Main St. is behind you.\nIt is not locked.", "", true)
+const room3 = new Room('Classroom', "\nYou're in the Classroom.\nBob, the instructor, is not currently here.\nHe's probably either making tea or skiing.\nYou could wait for him, but it might be a long wait.\nYou can go back down stairs, or you can stay here.", "")
 const room4 = new Room("Muddy Waters", "\nYou're inside Muddy Waters.\nIt's kind of dark since they keep the lights low.\nIt adds atmosphere.", "")
 const room5 = new Room("Mr Mikes", "\nYou're inside Mr Mikes.\nSmell the lovely odor of rancid, overcooked oil!\nBeware: if you stay here for more than 2 minutes,\nyour clothes will permanently smell like pizza grease.\nThere is something written on the specials board.", "")
-const room6 = new Room('MainSt1', "\nYou're outside 182 Main St.\n", "")
+
 const room7 = new Room('MainSt2', "\nYou're in front of Muddy Waters, one of Burlington's finest cafes.\nThey serve many types of coffee, tea and pastries.\nThe bathrooms also do not require a key!", "")
-const room8 = new Room('MainSt3', "\nYou're in front of Mr. Mikes, an ok pizza place that is mostly fequented by students.\nYou can get pizza here if you realllllly want,\nbut I wouldn't recommend it.\nTo your west is Muddy Waters.\nFurther west is 182 Main St. ", "")
+const room8 = new Room('MainSt3', "\nYou're in front of Mr. Mikes, an ok pizza place that is mostly fequented by students.\nYou can get pizza here if you realllllly want,\nbut I wouldn't recommend it.\nDown the street is 182 main st and Muddy Waters.", "")
 
 
 //--------------------------------------------------------------------------------------------------
 // Other Objects that need to be interacted with \\
 
 const sign = { message: "The password is 12345" }
-
+const rock = {}
+const paper = {}
 //--------------------------------------------------------------------------------------------------
 // Main Function \\
 
@@ -102,17 +102,14 @@ start();
 
 async function start() {
   player.name = await ask("Before we begin, what is your name?")
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  console.log(welcomeMessage);
+  const welcomeMessage = "Good evening fellow student! You're about to tour around our lovely campus.\nSome helpful information along your way:\nTyping i will bring up your inventory.\nTyping h will bring up your health.\nRemember to read the messages closely."
+  console.log(welcomeMessage)
+  console.log(room1.message);
   console.log('There is a rock on the ground')
 
-  while (answer !== 'Exit') {
+  while (answer !== 'exit') {
     answer = (await ask('>_')).trim().toLowerCase()
     player.health--
-    console.log(player.health)
     console.log(player.status())
 
     if (answer === 'read sign') {
@@ -122,13 +119,16 @@ On the door is a handwritten sign.`;
       console.log(player.health)
     }
     else if (answer === 'i') {
-
       console.log("You are carrying:\n" + player.inventory)
+    }
+    else if (answer === 'r'){
+      console.log(currentRoom)
     }
     else if (answer === "pick up rock") {
       let rock = room1.inventory.pop()
+
       player.inventory.push(rock)
-      
+
       console.log(player.inventory)
       console.log(room1.inventory)
     }
@@ -145,15 +145,11 @@ On the door is a handwritten sign.`;
       moveToRoom(room2)
       console.log(chalk.redBright.underline(room2.message))
     }
-    else if (answer === "go to 182") {
+    else if (answer === "go to 182 main st") {
       moveToRoom(room1)
       console.log(chalk.redBright.underline(room1.message))
     }
-    else if (answer === "to main st") {
-      moveToRoom(room6)
-      console.log(chalk.redBright.underline(room6.message))
-    }
-    else if (answer === "go east on main") {
+    else if (answer === "go to muddy waters") {
       moveToRoom(room7)
       console.log(chalk.redBright.underline(room7.message))
     }
@@ -167,17 +163,16 @@ On the door is a handwritten sign.`;
       } else {
         console.log("What next?")
       }
-
     }
     else if (answer === "exit muddy waters") {
       moveToRoom(room7)
       console.log(chalk.redBright.underline(room7.message))
     }
-    else if (answer === "end of main") {
+    else if (answer === "go to mr mikes" || answer === "go to mr. mikes") {
       moveToRoom(room8)
       console.log(chalk.redBright.underline(room8.message))
     }
-    else if (answer === "enter mr mikes") {
+    else if (answer === "enter mr mikes" || answer === "enter mr. mikes") {
       moveToRoom(room5)
       console.log(chalk.redBright.underline(room8.message))
       let pizzaQuestion = await ask("Would you like to buy some pizza?\n>_")
@@ -188,30 +183,17 @@ On the door is a handwritten sign.`;
         console.log("What next?")
       }
     }
-    else if (answer === "exit mr mikes") {
+    else if (answer === "exit mr mikes" || answer === "exit mr. mikes" || answer === "leave mr mikes" || answer === "leave mr. mikes") {
       moveToRoom(room8)
       console.log(chalk.redBright.underline(room8.message))
     }
-    else if (answer === "go west") {
-      moveToRoom(room7)
-      console.log(chalk.redBright.underline(room7.message))
-    }
-    else if (answer === "go west again") {
-      moveToRoom(room6)
-      console.log(chalk.redBright.underline(room6.message))
-    }
+  
     else {
       console.log("Sorry, I dont understand " + answer + "!")
     }
   }
   process.exit()
 }
-
 //--------------------------------------------------------------------------------------------------
 // In Game Functions \\
 
-//
-//function locked() {
-// if (room2.locked == true)
-//   console.log(chalk.bold.red('Bzzzzt Locked!'))
-//}
