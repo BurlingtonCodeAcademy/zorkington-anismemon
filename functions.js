fe13aff296b7ededfa9fa5a88aa2b460caa265c7
anismemon <58182830+anismemon@users.noreply.github.com>
2020-02-16 11:24:34 -0500

    improved add / drop functions to take any input

--- functions.js
+++ functions.js
@@ -1,48 +1,61 @@
 // take function to allow player to pick up items from rooms and add to own inventory
 
-// userInput = "take rock"
+answer = "drop the paper"
 
-let roomInv = ["table", "chair", "rock", "lamp"]
+let currentRoomInv = ["table", "chair", "rock", "lamp"]
 let playerInv = ["paper"]
 
-let item = "rock"
-takeItem()
 
-
-function takeItem() {
-    // let inputArray = userInput.split(" ")
-    // console.log(inputArray)
-    // let item = inputArray[1]
-    // console.log(item)
-    let n = roomInv.indexOf(item)
-    console.log(n)
-    let arrayItem = roomInv.splice(n, 1)
-    let roomItem = arrayItem.join()
-    playerInv.push(roomItem)
-    console.log(playerInv)
-    console.log(roomInv)
+if (answer.split(" ")[0] === 'take' || answer.split(" ")[0] === 'pick' || answer.split(" ")[0] === 'grab') {
+    takeItem()
+} else if (answer.split(" ")[0] === 'drop' || answer.split(" ")[0] === 'leave') {
+    dropItem()
 }
 
-
-// drop function to allow player to drop an item from his / her inventory, which is added to current room inventory
-
-// userInput = "drop rock"
-
-// let roomInv = ["table", "chair", "lamp"]
-// let playerInv = ["paper", "rock"]
-
-// function dropItem() {
-//     let inputArray = userInput.split(" ")
-//     console.log(inputArray)
-//     let item = inputArray[1]
-//     console.log(item)
-//     let n = playerInv.indexOf(item)
-//     console.log(n)
-//     let arrayItem = playerInv.splice(n, 1)
-//     let playerItem = arrayItem.join()
-//     roomInv.push(playerItem)
-//     console.log(playerInv)
-//     console.log(roomInv)
-// }
+function takeItem() {
+    let tempArray = []
+    for (let item of answer.split(" ")) {
 
 
+        if (currentRoomInv.includes(item)) {
+            let n = currentRoomInv.indexOf(item)
+            console.log(n)
+            let arrayItem = currentRoomInv.splice(n, 1)
+            let roomItem = arrayItem.join()
+            playerInv.push(roomItem)
+            console.log(playerInv)
+            console.log(currentRoomInv)
+            tempArray.push(item)
+            console.log(tempArray)
+
+
+        } else if (!currentRoomInv.includes(item)) {
+            tempArray = tempArray
+
+        }
+    } if (tempArray.length === 0) {
+        console.log("What you seek cannot be found.")
+    }
+}
+
+function dropItem() {
+    let tempArray = []
+    for (let item of answer.split(" ")) {
+        if (playerInv.includes(item)) {
+            let n = playerInv.indexOf(item)
+            console.log(n)
+            let arrayItem = playerInv.splice(n, 1)
+            let playerItem = arrayItem.join()
+            currentRoomInv.push(playerItem)
+            console.log(playerInv)
+            console.log(currentRoomInv)
+            tempArray.push(item)
+            console.log(tempArray)
+        } else if (!playerInv.includes(item)) {
+            tempArray = tempArray
+
+        }
+    } if (tempArray.length === 0) {
+        console.log("You cannot leave behind what you do not have.")
+    }
+}
