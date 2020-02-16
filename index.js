84a84dad42b0dd6a73dad914607405a314dd32d7
anismemon <58182830+anismemon@users.noreply.github.com>
2020-02-16 15:18:38 -0500

    added some details to muddy waters and mr mikes; cleanup up dangling console log checks; deleted completed tasks from to do list

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 // Tasks to be done \\
-// Create working lock function \\
 
 // Build story \\
 
 // Add inventory items to rooms \\
-
-// Figure out how to remove items from arrays \\
-// indexof splice \\ 
-
-// Read paper at muddy waters + 2 health points\\
-
-// change room names to actual room names\\
 
 //--------------------------------------------------------------------------------------------------
 
@@ -60,21 +52,21 @@ class Room {
   }
 }
 
-const main182 = new Room('182 Main St', "\nYou're at 182 Main St.\nThe home of the Burlington Code Academy!\n\nDown the street are Mr. Mikes and Muddy Waters.\nAhead of you is a door.\nOn the door is a hand written sign.\n", ["rock", "coin", "chewed gum"], true)
+const main182 = new Room('182 Main St', "\nYou're at 182 Main St.\nThe home of the Burlington Code Academy!\n\nDown the street are Mr. Mikes and Muddy Waters.\nAhead of you is a door.\nOn the door is a hand-written sign.\n", ["rock", "coin", "chewed gum"], true)
 const foyer = new Room('Foyer', "\nYou're in the foyer.\nThe foyer is a foyer.\nThere are stairs ahead of you.\nThe door to 182 Main St. is behind you.\nIt is not locked.", "", true)
 const classroom = new Room('Classroom', "\nYou're in the Classroom.\nBob, the instructor, is not currently here.\nHe's probably either making tea or skiing.\nYou could wait for him, but it might be a long wait.\nYou can go back down stairs, or you can stay here.", "")
-const muddyWaters = new Room("Muddy Waters", "\nYou're inside Muddy Waters.\nIt's kind of dark since they keep the lights low.\nIt adds atmosphere.", "")
-const mrMikes = new Room("Mr Mikes", "\nYou're inside Mr Mikes.\nSmell the lovely odor of rancid, overcooked oil!\nBeware: if you stay here for more than 2 minutes,\nyour clothes will permanently smell like pizza grease.\nThere is something written on the specials board.", "")
+const muddyWaters = new Room("Muddy Waters", "\nYou're inside Muddy Waters.\nIt's kind of dark since they keep the lights low.\nIt adds atmosphere.\nThere is a newspaper rack with the current\nedition of 7Days.", ["paper", "napkins"])
+const mrMikes = new Room("Mr Mikes", "\nYou're inside Mr Mikes.\nSmell the lovely odor of rancid, overcooked oil!\nBeware: if you stay here for more than 2 minutes,\nyour clothes will permanently smell like pizza grease.\nThere is something written on the specials board.", ["napkins"])
 const mainSt2 = new Room('MainSt2', "\nYou're in front of Muddy Waters, one of Burlington's finest cafes.\nThey serve many types of coffee, tea and pastries.\nThe bathrooms also do not require a key!", "")
 const mainSt3 = new Room('MainSt3', "\nYou're in front of Mr. Mikes, an ok pizza place that is mostly fequented by students.\nYou can get pizza here if you realllllly want,\nbut I wouldn't recommend it.\nDown the street are the Muddy Waters and the Burlington Code Academy.", "")
 
 
 //--------------------------------------------------------------------------------------------------
-// Other Objects that need to be interacted with \\
+//  objects that have messages \\
 
 const sign = { message: "The password is 12345" }
-const rock = {}
-const paper = {}
+const paper = { message: "The headline reads: 'President Sanders Changes Constitution to Become\nPresident For Life.'" }
+const mikesSpecials = { message: "The board says: 'We're out of anchovies today. Sorry.'" }
 //--------------------------------------------------------------------------------------------------
 // Main Function \\
 
@@ -112,7 +104,7 @@ async function start() {
   beginGame = await ask("Ready? Just hit enter to begin. ")
   console.log(main182.message);
   console.log('There is a shiny rock on the ground.')
-  console.log(player)
+  
 
   // this loop contains the entire game logic: 
 
@@ -135,10 +127,10 @@ async function start() {
     }
     else if (answer.split(" ")[0] === 'take' || answer.split(" ")[0] === 'pick' || answer.split(" ")[0] === 'grab') {
       takeItem()
-  }
+    }
     else if (answer.split(" ")[0] === 'drop' || answer.split(" ")[0] === 'leave') {
       dropItem()
-  }
+    }
     else if (answer === "open door" || answer === "enter 182 main") {
       console.log("Bzzzzzt!\n~Hint~\nMaybe try a password.")
     }
@@ -159,22 +151,42 @@ async function start() {
       moveToRoom(main182)
       console.log(/*chalk.redBright.underline*/(main182.message))
     }
-    else if (answer === "go to muddy waters") {
+    else if (answer === "go to muddy waters" || answer === "go to muddy") {
       moveToRoom(mainSt2)
       console.log(/*chalk.redBright.underline*/(mainSt2.message))
     }
-    else if (answer === "enter muddy waters") {
+    else if (answer === "enter muddy waters" || answer === "enter muddy") {
       moveToRoom(muddyWaters)
       console.log(/*chalk.redBright.underline*/(muddyWaters.message))
       let coffeeAnswer = await ask("Would you like to get a coffee?\n>_")
       if (coffeeAnswer === "yes") {
         player.health = player.health + 5
-        console.log("What next?")
+        console.log("That's sooooome java!")
       } else {
         console.log("What next?")
       }
     }
-    else if (answer === "exit muddy waters") {
+    else if (answer === "read paper" || answer === "open paper" || answer === "read 7 days" || answer === "open 7 days" || answer === "read 7days" || answer === "open 7days") {
+      console.log(paper.message)
+      console.log("In your excitement at the headline, you accidentally run your hand on some gum stuck under your chair.")
+      player.health = player.health + 2
+    }
+    else if (answer === "remove gum" || answer === "rub off gum" || answer === "try to remove gum" || answer === "wipe off gum" || answer === "clean hands" || answer === "clean gum" || answer === "clean off gum") {
+      console.log("That's some tenacious gum. You'll need to wash it off in the bathroom.\nAnd remember: you don't need a key!")
+    }
+    else if (answer === "use bathroom" && (currentRoom === muddyWaters || currentRoom === mrMikes) || answer === "go to the bathroom" && (currentRoom === muddyWaters || currentRoom === mrMikes) || answer === "go to bathroom" && (currentRoom === muddyWaters || currentRoom === mrMikes)) {
+      console.log("That's better! But remember: All employees must wash hands before returning to work...\nand so should you!")
+    }
+    else if (answer === "wash hands") {
+      console.log("Good hygiene is essential. Who wants to catch the corona virus?")
+    }
+    else if (answer === "use bathroom" && (currentRoom !== muddyWaters || currentRoom !== mrMikes) || answer === "go to the bathroom" && (currentRoom !== muddyWaters || currentRoom !== mrMikes)) {
+      console.log("Hey! It may be urgent, but please try to find a place that actually has a bathroom!")
+    }  
+    else if(answer === "drink coffee") {
+      console.log("That's better!")
+    }
+    else if (answer === "exit muddy waters" || answer === "exit muddy") {
       moveToRoom(mainSt2)
       console.log(/*chalk.redBright.underline*/(mainSt2.message))
     }
@@ -184,13 +196,16 @@ async function start() {
     }
     else if (answer === "enter mr mikes" || answer === "enter mr. mikes") {
       moveToRoom(mrMikes)
-      console.log(/*chalk.redBright.underline*/(mainSt3.message))
+      console.log(/*chalk.redBright.underline*/(mrMikes.message))
+    }
+    else if (answer === "read specials board" || answer === "read specials") {
+      console.log(mikesSpecials.message)
       let pizzaAnswer = await ask("Would you like to get some pizza?\n>_")
       if (pizzaAnswer === "yes") {
         player.health = player.health + 10
-        console.log("What next?")
+        console.log("I'd recommend stocking up on napkins.")
       } else {
-        console.log("What next?")
+        console.log("Okay, so What next?")
       }
     }
     else if (answer === "exit mr mikes" || answer === "exit mr. mikes" || answer === "leave mr mikes" || answer === "leave mr. mikes") {
@@ -219,14 +234,11 @@ function takeItem() {
 
     if (currentRoom.inventory.includes(item)) {
       let n = currentRoom.inventory.indexOf(item)
-      console.log(n)
       let arrayItem = currentRoom.inventory.splice(n, 1)
       let roomItem = arrayItem.join()
       player.inventory.push(roomItem)
-      console.log(player.inventory)
-      console.log(currentRoom.inventory)
       tempArray.push(item)
-      console.log(tempArray)
+      console.log(item[0].toUpperCase() + item.slice(1).toLowerCase() + " added to your inventory.")
 
     } else if (!currentRoom.inventory.includes(item)) {
       tempArray = tempArray
@@ -244,14 +256,11 @@ function dropItem() {
 
     if (player.inventory.includes(item)) {
       let n = player.inventory.indexOf(item)
-      console.log(n)
       let arrayItem = player.inventory.splice(n, 1)
       let playerItem = arrayItem.join()
       currentRoom.inventory.push(playerItem)
-      console.log(player.inventory)
-      console.log(currentRoom.inventory)
       tempArray.push(item)
-      console.log(tempArray)
+      console.log(item[0].toUpperCase() + item.slice(1).toLowerCase() + ' removed from your inventory.')
 
     } else if (!player.inventory.includes(item)) {
       tempArray = tempArray
@@ -261,7 +270,7 @@ function dropItem() {
   }
 }
 
-// allows user to unluck the door to the foyer and any other we choose to have locked
+// unlock function allows user to unluck the door to the foyer and any other we choose to have locked
 
 function unlock(room) {
   if (room.locked = true && answer === "input password" || answer === "12345") {
